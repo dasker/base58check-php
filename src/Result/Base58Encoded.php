b9ea93994fda1689ba8e2ac71f8bce367b77d563
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-03-09 02:36:16 +0500

    Base58Encoded result object

---
+++ src/Result/Base58Encoded.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is a part of "cryptolabs-pk/base58check-php" package.
+ * https://github.com/cryptolabs-pk/base58check-php
+ *
+ * Copyright (c) 2019 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/cryptolabs-pk/base58check-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace CryptoLabs\Base58\Result;
+
+use CryptoLabs\DataTypes\Buffer\AbstractStringType;
+
+/**
+ * Class Base58Encoded
+ * @package CryptoLabs\Base58\Result
+ */
+class Base58Encoded extends AbstractStringType
+{
+}
